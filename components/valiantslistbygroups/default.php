90a454ad671cb2b9aef6ee87c7a990a556cd2389
Mukola Trotsenko <newdarkstudiot@gmail.com>
2025-07-17 21:37:08 +0300

    Update default.php

--- components/valiantslistbygroups/default.php
+++ components/valiantslistbygroups/default.php
@@ -7,7 +7,7 @@
 		<div class="col-md-2 col-sm-4 col-xs-6">
 			   <div class="team-item">
 			       <div class="photo" style="background-image: url({{ valiant.avatar.path }});"></div>
-			       <a href="{{ ('/solder/' ~ valiant.slug)|url }}" class="name">
+			       <a href="{{ ('/profile/' ~ valiant.slug)|url }}" class="name">
 			           <span>{{valiant.fullName}}</span>
 			       </a>
 			       <div class="employment">
